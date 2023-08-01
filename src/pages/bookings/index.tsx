8e4d6ada50ea16711236e01fcc9d8ed1bf1b84d2
Phyo Yarzar <phyoyazar592@gmail.com>
2023-08-01 22:35:29 +0630

    add back button in bookings creatinng page

--- src/pages/bookings/index.tsx
+++ src/pages/bookings/index.tsx
@@ -7,7 +7,8 @@ import {
 	Text,
 	Title,
 } from '@mantine/core';
-import { IconPlus } from '@tabler/icons-react';
+import { IconArrowBack, IconPlus } from '@tabler/icons-react';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { ParcelCreate } from '~/features/booking/ParcelCreate';
 import { ReceiverCreate } from '~/features/booking/ReceiverCreate';
@@ -24,6 +25,8 @@ function Bookings() {
 
 	const [sender, setSender] = useState<SRType>({ id: '', name: '' });
 	const [receiver, setReceiver] = useState<SRType>({ id: '', name: '' });
+
+	const router = useRouter();
 
 	//*=========================================================================
 
@@ -49,71 +52,82 @@ function Bookings() {
 			return current < 3 ? current + 1 : current;
 		});
 
-	const prevStep = () =>
-		setActive((current) => (current > 0 ? current - 1 : current));
+	// const prevStep = () =>
+	// 	setActive((current) => (current > 0 ? current - 1 : current));
 
 	return (
 		<Center w={'100vw'}>
-			<Box w={800} mt={80}>
-				<Stepper active={active} breakpoint='sm'>
-					<Stepper.Step label='First step' description='Sender (You)'>
-						<SenderCreate
-							nextStep={nextStep}
-							townshipData={townshipData}
-							getSender={setSender}
-						/>
-					</Stepper.Step>
+			<Box w={800} mt={50}>
+				<Stack>
+					<Button
+						disabled={active !== 0}
+						w={100}
+						leftIcon={<IconArrowBack size={20} />}
+						variant='outline'
+						onClick={() => void router.back()}
+					>
+						Back
+					</Button>
+					<Stepper active={active} breakpoint='sm'>
+						<Stepper.Step label='First step' description='Sender (You)'>
+							<SenderCreate
+								nextStep={nextStep}
+								townshipData={townshipData}
+								getSender={setSender}
+							/>
+						</Stepper.Step>
 
-					<Stepper.Step label='Second step' description='Receiver (Customer)'>
-						<ReceiverCreate
-							nextStep={nextStep}
-							townshipData={townshipData}
-							getReceiver={setReceiver}
-						/>
-					</Stepper.Step>
+						<Stepper.Step label='Second step' description='Receiver (Customer)'>
+							<ReceiverCreate
+								nextStep={nextStep}
+								townshipData={townshipData}
+								getReceiver={setReceiver}
+							/>
+						</Stepper.Step>
 
-					<Stepper.Step label='Final step' description='Parcel Info'>
-						<ParcelCreate
-							sender={sender}
-							receiver={receiver}
-							nextStep={nextStep}
-						/>
-					</Stepper.Step>
+						<Stepper.Step label='Final step' description='Parcel Info'>
+							<ParcelCreate
+								sender={sender}
+								receiver={receiver}
+								nextStep={nextStep}
+							/>
+						</Stepper.Step>
 
-					<Stepper.Completed>
-						<Stack my={50}>
-							<Center>
-								<Title>Completed!</Title>
-							</Center>
-							<Center>
-								<Text c={'gray.6'} fz={'md'}>
-									You have successfully created the booking!
-								</Text>
-							</Center>
+						<Stepper.Completed>
+							<Stack my={50}>
+								<Center>
+									<Title>Completed!</Title>
+								</Center>
+								<Center>
+									<Text c={'gray.6'} fz={'md'}>
+										You have successfully created the booking!
+									</Text>
+								</Center>
 
-							<Center>
-								<Button
-									leftIcon={<IconPlus size={20} />}
-									onClick={() => {
-										setActive(0);
-										setSender({ name: '', id: '' });
-										setReceiver({ name: '', id: '' });
-									}}
-								>
-									Create Booking Again
-								</Button>
-							</Center>
-						</Stack>
-					</Stepper.Completed>
-				</Stepper>
+								<Center>
+									<Button
+										leftIcon={<IconPlus size={20} />}
+										onClick={() => {
+											setActive(0);
+											setSender({ name: '', id: '' });
+											setReceiver({ name: '', id: '' });
+										}}
+									>
+										Create Booking Again
+									</Button>
+								</Center>
+							</Stack>
+						</Stepper.Completed>
+					</Stepper>
 
-				{/* <Group position='right' mt='xl'>
+					{/* <Group position='right' mt='xl'>
 					{active !== 0 && (
 						<Button variant='default' onClick={prevStep}>
 							Back
 						</Button>
 					)}
 				</Group> */}
+				</Stack>
 			</Box>
 		</Center>
 	);
